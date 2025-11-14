c5a95c16af984a163ffbb3656c96cee1dcafbe3f
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2025-11-14 14:05:20 +0100

    finished class SQL 14 Nov

--- code_along/week1_query_bikedata/sql/query_bike.sql
+++ code_along/week1_query_bikedata/sql/query_bike.sql
@@ -51,3 +51,78 @@ FROM staging.joined_table j
 JOIN staging.status s ON j.order_status = s.order_status
 
 -- sort the rows by order status
+-- ORDER BY... ASC DESC
+SELECT
+  j.order_id,
+  j.order_status,
+  s.order_status_description
+FROM staging.joined_table j
+JOIN staging.status s ON j.order_status = s.order_status
+-- ORDER BY j.order_status DESC;
+ORDER BY j.order_status ASC;
+
+/*  ============================
+    investigate unique 
+    customers
+    ============================ */
+
+-- DISTINCT
+SELECT DISTINCT order_id
+FROM staging.joined_table
+ORDER BY order_id DESC;
+
+-- find unique values of customer_id
+SELECT DISTINCT customer_id
+FROM staging.joined_table
+ORDER BY customer_id ASC;
+
+-- find unique values of customer full names
+SELECT DISTINCT customer_first_name, customer_last_name
+FROM staging.joined_table
+ORDER BY customer_first_name, customer_last_name
+
+-- it is 'Justina Jenkins' that is the issue
+-- this can be found out by one window function
+SELECT 
+  customer_id,
+  customer_first_name,
+  customer_last_name,
+  customer_city
+FROM staging.joined_table
+WHERE customer_first_name = 'Justina' AND 
+customer_last_name = 'Jenkins'
+
+/*  ===========
+    Introduce
+    Aggregation
+    =========== */
+-- aggregate over rows
+-- there are different ways of aggregation (max, min, avg, sum, count)
+
+-- what is the total revenue from all order
+SELECT
+  ROUND(SUM(quantity*list_price)) AS total_revenue
+FROM staging.joined_table;
+
+-- try out other aggregation functions
+SELECT
+  ROUND(MIN(quantity*list_price)) AS min_revenue,
+  ROUND(MAX(quantity*list_price)) AS max_revenue
+FROM staging.joined_table;
+
+/*  ===========
+    CASE...WHEN
+    =========== */
+-- similar if...else in other languages
+
+-- we can replace the order status column to some descriptions
+SELECT
+  order_id,
+  product_name,
+  CASE order_status
+    WHEN 1 THEN 'Pending'
+    WHEN 2 THEN 'Processing'
+    WHEN 3 THEN 'Rejected'
+    WHEN 4 THEN 'Completed'
+  END AS order_status_description
+FROM staging.joined_table;
