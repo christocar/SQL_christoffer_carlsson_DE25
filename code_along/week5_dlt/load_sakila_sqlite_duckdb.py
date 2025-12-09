e77ef7f9a57646d7937be84bc6acf1246534c3e6
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2025-12-09 20:52:23 +0100

    save before adjusting load sakila

---
+++ code_along/week5_dlt/load_sakila_sqlite_duckdb.py
@@ -0,0 +1,17 @@
+import dlt
+from dlt.sources.sqlite import sql_database
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent / "data"
+SQLITE_PATH = DATA_PATH / "sqlite-sakila.db"
+DUCKDB_PATH = DATA_PATH / "sakila.duckdb"
+
+source = sql_database(credentials=f"sqlite:///{SQLITE_PATH}", schema="main")
+
+pipeline = dlt.pipeline(
+    pipeline_name="sakila_sqlite_to_duckdb", 
+    destination=dlt.destinations.duckdb(str(DUCKDB_PATH)), 
+    dataset_name="staging",
+)
+
+load_info = pipeline.run(source, write_disposition="replace")
